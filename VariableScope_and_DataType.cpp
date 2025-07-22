24e387f41d39ca027e21d6a263deac8837bfefbf
sayyamworks <sardarsayyam027@gmail.com>
2025-07-22 09:51:35 +0500

    Commit C++

---
+++ VariableScope_and_DataType.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int age = 22;
+void data()
+{
+    cout<<"Age is: "<<age<<endl;
+}
+int main()
+{
+    int age = 23; //local variable
+    cout<<age<<endl;
+    float num1= 23.4;
+    double pi = 3.14777777;
+    char section = 'A';
+    bool condition = true;
+    string name = "Sardar Sayyam Ijaz";
+    cout<<num1<<endl;
+    cout<<pi<<endl;
+    cout<<section<<endl;
+    cout<<condition<<endl;
+    cout<<name<<endl;
+    data(); //Global Variable Call
+}
---
+++ input/input_output.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Enter your details."<<endl;
+    string name;
+    cout<<"Enter your Name: "<<endl;
+    cin>>name;
+    int age;
+    cout<<"Enter your Age: "<<endl;
+    cin>>age;
+    cout<<"Hello! Mr "<<name<<" You are just "<<age<<" years Old. Right! ";
+}
