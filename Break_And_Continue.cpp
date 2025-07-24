309aae054edd46c9ef5a87599841fa655bab12b3
sayyamworks <sardarsayyam027@gmail.com>
2025-07-25 00:17:39 +0500

    Break And Continue.

---
+++ Break_And_Continue.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    //Break.
+    for(int i=0;i<=10;i++)
+    {
+        if(i==7)
+        {
+            break;
+        }
+        cout<<i<<endl;
+    }
+    */
+    //Continue.
+    for(int i=0;i<=10;i++)
+    {
+        if(i==7)
+        {
+            continue;
+        }
+        cout<<i<<endl;
+    }
+    return 0;
+}
